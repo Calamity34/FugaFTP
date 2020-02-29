eed2ed5adb060b237a3da13d9e6da4ea341106cf
KrutosVIP <KrutosVIP@users.noreply.github.com>
2020-02-29 19:10:07 +0400

    Php chat fix

--- phps/chat.php
+++ phps/chat.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html lang = "ru">
   <head>
+    <header>
+      <a href="/">
+        <img src="..\images\fuga1.png" alt="Logo of fuga"/>
+        <h1>Fuga Site</h1>
+      </a>
+    </header>
     <meta http-equiv=Content-Type content="text/html;charset=utf-8">
     <h1 class = "main-h">Test Chat</h1>
     <style>
@@ -20,12 +26,18 @@
       color: #2ec6e8
     }
     </style>
-    <link rel="icon" href="images\fuga1.png" type="image/x-icon">
-    <link rel="shortcut icon" href="images\fuga1.png" type="image/x-icon">
+    <link rel="icon" href="..\images\fuga1.png" type="image/x-icon">
+    <link rel="shortcut icon" href="..\images\fuga1.png" type="image/x-icon">
   </head>
   <body>
     <div class="scrollings">
       А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации.
     </div>
+    <form name="form" action="" method="get">
+      <input type="text" name="chat" id="chat" value="Any text?">
+    </form>
+    <?php
+
+    ?>
   </body>
 </html>
