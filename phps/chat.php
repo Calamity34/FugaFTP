83b19a6bae3434b64c38271c00fcbba782a05796
KrutosVIP <KrutosVIP@users.noreply.github.com>
2020-02-29 19:02:51 +0400

    PHP chat
    Added first test for chat

---
+++ phps/chat.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang = "ru">
+  <head>
+    <meta http-equiv=Content-Type content="text/html;charset=utf-8">
+    <h1 class = "main-h">Test Chat</h1>
+    <style>
+    .scrollings {
+    height: 200px; /* высота нашего блока */
+    width: 200px; /* ширина нашего блока */
+    background: #fff; /* цвет фона, белый */
+    border: 1px solid #C1C1C1; /* размер и цвет границы блока */
+    overflow-x: scroll; /* прокрутка по горизонтали */
+    overflow-y: scroll; /* прокрутка по вертикали */
+    }
+    h1.main-h{
+      font-weight: bolder;
+      color: #d6c424
+    }
+    body {
+      color: #2ec6e8
+    }
+    </style>
+    <link rel="icon" href="images\fuga1.png" type="image/x-icon">
+    <link rel="shortcut icon" href="images\fuga1.png" type="image/x-icon">
+  </head>
+  <body>
+    <div class="scrollings">
+      А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации. А тут много-много разного текста и прочей информации.
+    </div>
+  </body>
+</html>
---
+++ phps/index.php
@@ -0,0 +1,3 @@
+<?php
+  include_once "phps/chat.php"
+?>
