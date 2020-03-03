5896ad2289f7cf52efb3fe7efbce6cb1b3183792
Calamity <NikitaYoutube2008@icloud.com>
2020-03-03 21:07:06 +0500

    Image Name Change

--- phps/chat.php
+++ phps/chat.php
@@ -4,14 +4,14 @@
   <head>
     <header>
       <a href="/">
-        <img src="..\images\fuga1.png" alt="Fuga Logo"/>
+        <img src="..\images\favicon.png" alt="Fuga Logo"/>
         <h1>Fuga Site</h1>
       </a>
     </header>
     <meta http-equiv=Content-Type content="text/html;charset=utf-8">
     <link rel="stylesheet" type="text/css" href="../css/phpchatstyle.css">
-    <link rel="icon" href="..\images\fuga1.png" type="image/x-icon">
-    <link rel="shortcut icon" href="..\images\fuga1.png" type="image/x-icon">
+    <link rel="icon" href="..\images\favicon.png">
+    <link rel="shortcut icon" href="..\images\favicon.png" type="image/x-icon">
     <h1 class = "main-h">Test Chat</h1>
   </head>
   <body>
