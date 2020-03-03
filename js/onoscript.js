ea45d7b29b4fdfa0c265c644498635153bfaae27
Calamity <NikitaYoutube2008@icloud.com>
2020-03-03 18:01:10 +0500

    i hate you, sergey

--- js/onoscript.js
+++
@@ -1,6 +0,0 @@
-console.log("ono(); - запусти, тебе понравится!");
-function ono(){
-console.log("ура. послушай это...");
-var audio = new Audio('sounds/verynicemusiciloveit.wav');
-audio.play();
-}
