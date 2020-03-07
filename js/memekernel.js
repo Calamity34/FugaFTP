183a5e98f090ef7c436669402e41aeca15bc3c6a
SergeyMC9730 <57207009+SergeyMC9730@users.noreply.github.com>
2020-03-07 22:08:32 +0800

    Меме страница

---
+++ js/memekernel.js
@@ -0,0 +1,112 @@
+			console.log("Инцилизация скрипта");
+function GettingRMV(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+console.log("Инцилизация функции GettingRMV. Рандомайзинг.");
+var a = GettingRMV(27);
+console.warn(a);
+function EndingRMV(){
+	console.log("Инцилизация функции EndingRMV. Проверка данных.");
+	if(a == 1){
+		var player = new Playerjs({id:"player", file:"memes/meme1.mp4"});
+		console.log("1");
+	}if(a == 2){
+		var player = new Playerjs({id:"player", file:"memes/meme2.mp4"});
+		console.log("1");
+	}if(a == 3){
+		var player = new Playerjs({id:"player", file:"memes/meme3.mp4"});
+		console.log("1");
+	}if(a == 4){
+		var player = new Playerjs({id:"player", file:"memes/meme4.mp4"});
+		console.log("1");
+	}if(a == 5){
+		var player = new Playerjs({id:"player", file:"memes/meme5.mp4"});
+		console.log("1");
+	}if(a == 6){
+		var player = new Playerjs({id:"player", file:"memes/meme6.mp4"});
+		console.log("1");
+	}if(a == 7){
+		var player = new Playerjs({id:"player", file:"memes/meme7.mp4"});
+		console.log("1");
+	}if(a == 8){
+		var player = new Playerjs({id:"player", file:"memes/meme8.mp4"});
+		console.log("1");
+	}if(a == 9){
+		var player = new Playerjs({id:"player", file:"memes/meme9.mp4"});
+		console.log("1");
+	}if(a == 10){
+		var player = new Playerjs({id:"player", file:"memes/meme10.mp4"});
+		console.log("1");
+	}if(a == 11){
+		var player = new Playerjs({id:"player", file:"memes/meme11.mp4"});
+		console.log("1");
+	}if(a == 12){
+		var player = new Playerjs({id:"player", file:"memes/meme12.mp4"});
+		console.log("1");
+	}if(a == 13){
+		var player = new Playerjs({id:"player", file:"memes/meme13.mp4"});
+		console.log("1");
+	}if(a == 14){
+		var player = new Playerjs({id:"player", file:"memes/meme14.mp4"});
+		console.log("1");
+	}if(a == 15){
+		var player = new Playerjs({id:"player", file:"memes/meme15.mp4"});
+		console.log("1");
+	}if(a == 16){
+		var player = new Playerjs({id:"player", file:"memes/meme16.mp4"});
+		console.log("1");
+	}if(a == 17){
+		var player = new Playerjs({id:"player", file:"memes/meme17.mp4"});
+		console.log("1");
+	}if(a == 18){
+		var player = new Playerjs({id:"player", file:"memes/meme18.mp4"});
+		console.log("1");
+	}if(a == 19){
+		var player = new Playerjs({id:"player", file:"memes/meme19.mp4"});
+		console.log("1");
+	}if(a == 20){
+		var player = new Playerjs({id:"player", file:"memes/meme20.mp4"});
+		console.log("1");
+	}if(a == 21){
+		var player = new Playerjs({id:"player", file:"memes/meme21.mp4"});
+		console.log("1");
+	}if(a == 22){
+		var player = new Playerjs({id:"player", file:"memes/meme22.mp4"});
+		console.log("1");
+	}if(a == 23){
+		var player = new Playerjs({id:"player", file:"memes/meme23.mp4"});
+		console.log("1");
+	}if(a == 24){
+		var player = new Playerjs({id:"player", file:"memes/meme24.mp4"});
+		console.log("1");
+	}if(a == 25){
+		var player = new Playerjs({id:"player", file:"memes/meme25.mp4"});
+		console.log("1");
+	}if(a == 26){
+		var player = new Playerjs({id:"player", file:"memes/meme26.mp4"});
+		console.log("1");
+	}if(a == 27){
+		var player = new Playerjs({id:"player", file:"memes/meme27.mp4"});
+		console.log("1");
+	}if(a == 28){
+		var player = new Playerjs({id:"player", file:"memes/meme28.mp4"});
+		console.log("1");
+	}if(a == 29){
+		var player = new Playerjs({id:"player", file:"memes/meme29.mp4"});
+		console.log("1");
+	}if(a == 30){
+		var player = new Playerjs({id:"player", file:"memes/meme30.mp4"});
+		console.log("1");
+	}if(a == 31){
+		var player = new Playerjs({id:"player", file:"memes/meme31.mp4"});
+		console.log("1");
+	}if(a == 32){
+		var player = new Playerjs({id:"player", file:"memes/meme32.mp4"});
+		console.log("1");
+	}if(a == 33){
+		var player = new Playerjs({id:"player", file:"memes/meme33.mp4"});
+		console.log("1");
+	}
+	console.warn("1");
+}
+EndingRMV();
