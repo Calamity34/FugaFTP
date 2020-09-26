50962afe188d1a621e4350d8d98bd0b0df4dd824
SergeyMC9730 <57207009+SergeyMC9730@users.noreply.github.com>
2020-09-26 11:58:05 +0800

    Update functionscripts.js

--- js/functionscripts.js
+++ js/functionscripts.js
@@ -1,15 +1,17 @@
-console.log("ono(); - запусти, тебе понравится!");
-console.log("также попробуй - bruh()");
+console.log("it(); - запусти, тебе понравится!");
+console.log("также попробуй - bruh();");
 
-function ono(){
-console.log("ура. послушай это...");
-var audio = new Audio('sounds/verynicemusiciloveit.wav');
-audio.play();
+function it(){
+    console.log("ура. послушай это...");
+    document.write("I T  2");
+    var audio = new Audio('sounds/verynicemusiciloveit.wav');
+    audio.play();
 }
 
 function bruh(){
     var i = 0;
-    while (i<100){ // change "100" to any number to depend the bruhs amount
+    var rnd = Math.floor(Math.random() * Math.floor(Math.floor(Math.random() * Math.floor(150))));
+    while (i < rnd){ // change "100" to any number to depend the bruhs amount
         i++;
         document.write("BRUH ");
     }
