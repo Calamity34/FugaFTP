c8b4eec6016c04e5a25bfc844cf702caa192830b
SergeyMC9730 <57207009+SergeyMC9730@users.noreply.github.com>
2020-09-26 11:53:26 +0800

    Update memekernel.js

--- js/memekernel.js
+++ js/memekernel.js
@@ -1,13 +1,1 @@
-console.log("Инцилизация скрипта");
-function GettingRMV(max) {
-	console.log("Инцилизация функции GettingRMV. Рандомайзинг.");
-	return Math.floor(Math.random() * Math.floor(max));
-}
-var a = GettingRMV(70);
-console.warn(a);
-function EndingRMV(){
-	console.log("Инцилизация функции EndingRMV. Проверка данных.");
-	var player = new Playerjs({id:"player", file:"memes/meme"+a+".mp4"});
-	console.warn("1");
-}
-EndingRMV();
+var player = new Playerjs({id:"player", file:"memes/meme" + Math.floor(Math.random() * Math.floor(70)) + ".mp4"});
